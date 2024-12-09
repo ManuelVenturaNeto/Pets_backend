e29d1821ebdb5ba34ecbaf7cdda8e6c4eff345f1
Manuel Ventura <manuueelneto@gmail.com>
2024-12-09 15:32:05 -0300

    feat: implemante register use case

--- src/data/register_user/register_test.py
+++ src/data/register_user/register_test.py
@@ -29,3 +29,28 @@ def test_register():
     # Testing outputs
     assert response["Success"] is True
     assert response["Data"]
+
+
+def test_register_fail():
+    """
+    Testing registry method
+    """
+    user_repo = UserRepositorySpy()
+    register_user = RegisterUser(user_repo)
+
+    attributes = {
+        "name": faker.random_number(digits=5),
+        "password": faker.password(),
+    }
+
+    response = register_user.register(
+        name=attributes["name"],
+        password=attributes["password"],
+    )
+
+    # Testing inputs
+    assert user_repo.insert_user_params == {}  # pylint: disable=C1803
+
+    # Testing outputs
+    assert response["Success"] is False
+    assert response["Data"] is None
