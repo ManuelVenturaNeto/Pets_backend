2c013c2855c7938ef668178a8e3224e1889c65e3
Manuel Ventura <manuueelneto@gmail.com>
2024-12-11 11:14:11 -0300

    feat: implemante find user usecase and testing it

--- src/data/find_user/find_test.py
+++ src/data/find_user/find_test.py
@@ -48,3 +48,27 @@ def test_by_name():
     # Testing outputs
     assert response["Success"] is True
     assert response["Data"]
+
+
+def test_by_id_and_name():
+    """
+    Testing by_id_and_name method
+    """
+
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    attibutes = {
+        "id": faker.random_number(digits=5),
+        "name": faker.name(),
+    }
+
+    response = find_user.by_id_and_name(user_id=attibutes["id"], name=attibutes["name"])
+
+    # Testing inputs
+    assert user_repo.select_user_params["user_id"] == attibutes["id"]
+    assert user_repo.select_user_params["name"] == attibutes["name"]
+
+    # Testing outputs
+    assert response["Success"] is True
+    assert response["Data"]
