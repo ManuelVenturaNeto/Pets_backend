147a3b4ec8e670eaca92d73bc837ab925bef0e5a
Manuel Ventura <manuueelneto@gmail.com>
2024-12-12 11:22:32 -0300

    feat: implemante find pet usecase and testing it

--- src/data/find_pet/find_test.py
+++ src/data/find_pet/find_test.py
@@ -24,3 +24,49 @@ def test_by_pet_id():
     # testing output
     assert response["Success"] is True
     assert response["Data"]
+
+
+def test_by_user_id():
+    """
+    Testing by_user_id method
+    """
+
+    pet_repo = PetRepositorySpy()
+    find_pet = FindPet(pet_repo)
+
+    attributes = {"user_id": faker.random_number(digits=5)}
+
+    response = find_pet.by_user_id(user_id=attributes["user_id"])
+
+    # testing input
+    assert pet_repo.select_pet_param["user_id"] == attributes["user_id"]
+
+    # testing output
+    assert response["Success"] is True
+    assert response["Data"]
+
+
+def test_by_pet_id_and_user_id():
+    """
+    Testing by_pet_id_and_user_id method
+    """
+
+    pet_repo = PetRepositorySpy()
+    find_pet = FindPet(pet_repo)
+
+    attributes = {
+        "pet_id": faker.random_number(digits=5),
+        "user_id": faker.random_number(digits=5),
+    }
+
+    response = find_pet.by_pet_id_and_user_id(
+        pet_id=attributes["pet_id"], user_id=attributes["user_id"]
+    )
+
+    # testing input
+    assert pet_repo.select_pet_param["pet_id"] == attributes["pet_id"]
+    assert pet_repo.select_pet_param["user_id"] == attributes["user_id"]
+
+    # testing output
+    assert response["Success"] is True
+    assert response["Data"]
