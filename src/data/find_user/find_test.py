9a35c0d8e792a1a251566c8191aa125113f53262
Manuel Ventura <manuueelneto@gmail.com>
2024-12-12 12:09:50 -0300

    feat: implementing in tests of find pet adn tests of find user the fail tests

--- src/data/find_user/find_test.py
+++ src/data/find_user/find_test.py
@@ -28,6 +28,28 @@ def test_by_id():
     assert response["Data"]
 
 
+def test_by_id_fail():
+    """
+    Testing by_id method
+    """
+
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    attibutes = {
+        "id": faker.name(),
+    }
+
+    response = find_user.by_id(user_id=attibutes["id"])
+
+    # Testing inputs
+    assert user_repo.select_user_params == {}  # pylint: disable=C1803
+
+    # Testing outputs
+    assert response["Success"] is False
+    assert response["Data"] is None
+
+
 def test_by_name():
     """
     Testing by_name method
@@ -48,6 +70,28 @@ def test_by_name():
     # Testing outputs
     assert response["Success"] is True
     assert response["Data"]
+
+
+def test_by_name_fail():
+    """
+    Testing by_name method
+    """
+
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    attibutes = {
+        "name": faker.random_number(digits=5),
+    }
+
+    response = find_user.by_name(name=attibutes["name"])
+
+    # Testing inputs
+    assert user_repo.select_user_params == {}  # pylint: disable=C1803
+
+    # Testing outputs
+    assert response["Success"] is False
+    assert response["Data"] is None
 
 
 def test_by_id_and_name():
@@ -72,3 +116,27 @@ def test_by_id_and_name():
     # Testing outputs
     assert response["Success"] is True
     assert response["Data"]
+
+
+def test_by_id_and_name_fail():
+    """
+    Testing by_id_and_name method
+    """
+
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    attibutes = {
+        "id": faker.name(),
+        "name": faker.random_number(digits=5),
+    }
+
+    response = find_user.by_id_and_name(user_id=attibutes["id"], name=attibutes["name"])
+
+    # Testing inputs
+    assert user_repo.select_user_params == {}  # pylint: disable=C1803
+    assert user_repo.select_user_params == {}  # pylint: disable=C1803
+
+    # Testing outputs
+    assert response["Success"] is False
+    assert response["Data"] is None
